31aad67da063c484091400de61ebd586ee92b87c
Souma <souma.hitech@gmail.com>
2020-08-20 21:00:05 +0530

    add react hooks and interval function to display current time.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React,{useState}from "react";
 
 function App() {
+
+  const now = new Date().toLocaleTimeString();
+  // N:B you have to use hocks 
+  // destructure array
+  const [currentTime, setTime] = useState(now);
+  function updateTime(){
+    setTime(new Date().toLocaleTimeString());
+  }
+
+  // 
+  setInterval(updateTime,1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
